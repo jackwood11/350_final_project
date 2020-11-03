c956758003b98878e0b093479fd531e3f0d9d823
Tess Lipsky <tesslipsky@Tesss-MacBook-Air.local>
2020-11-03 17:19:00 -0500

    LSFR trials

---
+++ LFSR_TB.v
@@ -0,0 +1,23 @@
+module LFSR_TB ();
+ 
+  parameter c_NUM_BITS = 4;
+   
+  reg r_Clk = 1'b0;
+   
+  wire [c_NUM_BITS-1:0] w_LFSR_Data;
+  wire w_LFSR_Done;
+   
+  LFSR #() LFSR_inst
+         (.i_Clk(r_Clk),
+          .i_Enable(1'b1),
+          .i_Seed_DV(1'b0),
+          .i_Seed_Data({c_NUM_BITS{1'b0}}), // Replication
+          .o_LFSR_Data(w_LFSR_Data),
+          .o_LFSR_Done(w_LFSR_Done), 
+          .NUM_BITS(c_NUM_BITS)
+          );
+  
+  always @(*)
+    #10 r_Clk <= ~r_Clk; 
+   
+endmodule // LFSR_TB
---
+++ LFSR_orig.v
@@ -0,0 +1,9 @@
+module LFSR(NUM_BITS, clk, en, seed, rand_num); 
+
+    input 
+
+endmodule 
+
+
+  
+
